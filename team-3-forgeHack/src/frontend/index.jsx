a6aa9a624e6da71d8e9e15282c872fdea04ff749
SamuelPullukaran <samuel.pullukaran5@gmail.com>
2024-07-02 15:43:38 +1000

    basic outline

--- team-3-forgeHack/src/frontend/index.jsx
+++ team-3-forgeHack/src/frontend/index.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ForgeReconciler, { Text } from "@forge/react";
-import ForgeUI, { render, AvatarStack, Avatar } from "@forge/ui";
+import { ProgressTracker } from "@forge/react";
+import { Stack } from "@forge/react";
 import { Box } from "@forge/react";
 import { invoke } from "@forge/bridge";
-import { Label, Textfield, TextArea } from "@forge/react";
+import { Label, Textfield, TextArea, xcss } from "@forge/react";
 import { view } from "@forge/bridge";
 import { Spinner } from "@forge/react";
 import {
@@ -19,25 +20,113 @@ import {
 } from "@forge/react";
 
 const App = () => {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
+  // const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
-    invoke("getText", { example: "my-invoke-variable" }).then(setData);
+    // Grab data of buttons in json object
+    // invoke("getText", { example: "my-invoke-variable" }).then(setData);
   }, []);
+
+  const data = [
+    {
+      name: "AIF",
+      logo: "image",
+    },
+    {
+      name: "British Asian Trust",
+      logo: "image",
+    },
+  ];
 
   return (
     <>
-      <Box />
-      <AvatarStack>
-        <Avatar accountId="5a1234bc8d12345e3f1g11hi" />
-        <Avatar accountId="2a98a42dbc7ab42e12ee360d" />
-        <Avatar accountId="5d8732lq8jg85a0e3f1g90as" />
-        <Avatar accountId="2h98a10dbl5ab93e62hja23z" />
-        <Avatar accountId="7b20f0bc2d05325e3f1g43ty" />
-        <Avatar accountId="2p72s42dbc7ab42e90gf252d" />
-        <Avatar accountId="2l01x78mf4pqw42e84fg40ad" />
-      </AvatarStack>
+      {/* <Button
+        onClick={() => setClicked((prev) => !prev)}
+        shouldFitContainer
+        appearance="primary"
+      >
+        America India Foundation
+      </Button> */}
+
+      <Stack alignInline="center" alignBlock="center">
+        <Box xcss={xcss({ marginBottom: "space.150" })}>
+          <Heading as="h1">Foundation Fundraiser !!</Heading>
+        </Box>
+
+        <Stack alignInline="center" alignBlock="center" space="space.200">
+          <ProgressTrackerExample />
+          {/* <Stack grow="fill" space="space.200"> */}
+          {data.map((fund) => (
+            <FundButton name={fund.name} />
+          ))}
+        </Stack>
+        {/* </Stack> */}
+      </Stack>
     </>
+  );
+};
+
+const FundButton = ({ name }) => {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <>
+      <Button
+        onClick={() => setClicked((prev) => !prev)}
+        shouldFitContainer
+        appearance="primary"
+      >
+        {name}
+      </Button>
+      {clicked && "THIS IS A TEST"}
+    </>
+  );
+};
+
+const ProgressTrackerExample = () => {
+  return (
+    <ProgressTracker
+      xcss={{ paddingBottom: "space.200" }}
+      items={[
+        {
+          id: "1",
+          label: "Disabled step",
+          percentageComplete: 100,
+          status: "disabled",
+        },
+        {
+          id: "2",
+          label: "Create a space",
+          percentageComplete: 100,
+          status: "visited",
+        },
+        {
+          id: "3",
+          label: "Upload a photo",
+          percentageComplete: 0,
+          status: "current",
+        },
+        {
+          id: "4",
+          label: "Your details",
+          percentageComplete: 0,
+          status: "unvisited",
+        },
+        {
+          id: "5",
+          label: "Invite users",
+          percentageComplete: 0,
+          status: "unvisited",
+        },
+        {
+          id: "6",
+          label: "Confirm",
+          percentageComplete: 0,
+          status: "unvisited",
+        },
+      ]}
+    />
   );
 };
 
