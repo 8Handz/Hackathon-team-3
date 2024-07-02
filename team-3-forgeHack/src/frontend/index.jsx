c4717699173240872559034fd6091b9cd6a5dffa
etkintetik <etkintetik101@gmail.com>
2024-07-02 15:49:26 +1000

    added a page reader

--- team-3-forgeHack/src/frontend/index.jsx
+++ team-3-forgeHack/src/frontend/index.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ForgeReconciler, { Text } from "@forge/react";
-import ForgeUI, { render, AvatarStack, Avatar } from "@forge/ui";
+import { requestConfluence } from '@forge/bridge';
+// import ForgeUI, { render, AvatarStack, Avatar } from "@forge/ui";
 import { Box } from "@forge/react";
 import { invoke } from "@forge/bridge";
-import { Label, Textfield, TextArea } from "@forge/react";
+import { Label, Textfield, TextArea, User, UserGroup } from "@forge/react";
 import { view } from "@forge/bridge";
 import { Spinner } from "@forge/react";
 import {
@@ -18,25 +19,40 @@ import {
   useForm,
 } from "@forge/react";
 
+const fetchPageContent = async () => {
+  const res = await requestConfluence('/wiki/api/v2/pages/2097194?body-format=storage');
+  console.log(res);
+  const data = await res.json();
+  console.log(data);
+  console.log(data.body.storage.value);
+  return data.body.storage.value;
+}
+
 const App = () => {
   const [data, setData] = useState(null);
+  const [pageData, setPageData] = useState(null);
+
+  // fetchPageContent().then(setPageData).then(console.log(pageData));
 
   useEffect(() => {
+    fetchPageContent().then(setPageData);
     invoke("getText", { example: "my-invoke-variable" }).then(setData);
   }, []);
 
   return (
     <>
-      <Box   />
-      <AvatarStack>
-        <Avatar accountId="5a1234bc8d12345e3f1g11hi" />
-        <Avatar accountId="2a98a42dbc7ab42e12ee360d" />
-        <Avatar accountId="5d8732lq8jg85a0e3f1g90as" />
-        <Avatar accountId="2h98a10dbl5ab93e62hja23z" />
-        <Avatar accountId="7b20f0bc2d05325e3f1g43ty" />
-        <Avatar accountId="2p72s42dbc7ab42e90gf252d" />
-        <Avatar accountId="2l01x78mf4pqw42e84fg40ad" />
-      </AvatarStack>
+      {/* <Box   /> */}
+      <Text>Hello</Text>
+      <UserGroup>
+        <User accountId="5a1234bc8d12345e3f1g11hi" />
+        <User accountId="2a98a42dbc7ab42e12ee360d" />
+        <User accountId="5d8732lq8jg85a0e3f1g90as" />
+        <User accountId="2h98a10dbl5ab93e62hja23z" />
+        <User accountId="7b20f0bc2d05325e3f1g43ty" />
+        <User accountId="2p72s42dbc7ab42e90gf252d" />
+        <User accountId="2l01x78mf4pqw42e84fg40ad" />
+      </UserGroup>
+      <Text>{pageData || 'loading.....'}</Text>
     </>
   );
 };
